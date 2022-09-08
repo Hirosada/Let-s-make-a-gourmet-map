fd9bf50f2affdd623835f2b70567f12888720402
NaganoHirosada <>
2022-09-08 17:50:15 +0900

    修正

--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -1,7 +1,7 @@
 
 @extends('layouts')
 @section('contents')
-	{{ Form::open(['route' => 'post_regiser', 'method' => 'post']) }}
+	{{ Form::open(['route' => 'post_regiser', 'method' => 'post', 'class' => 'member-form']) }}
 		<div class="inputs">
 			{{ Form::text('text', null, ['placeholder' => 'お名前', 'name' => 'member_name', 'class' => 'input_form']) }}
 			<i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
@@ -11,21 +11,21 @@
 			<i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
 		</div>
 		<div class="inputs">
-			{{ Form::password('password', null, ['placeholder' => 'パスワード', 'name' => 'member_password', 'class' => 'input_form']) }}
+			{{ Form::password('password', ['placeholder' => 'パスワード', 'name' => 'member_password', 'class' => 'input_form']) }}
 			<i class="fa fa-unlock-alt f13e" aria-hidden="true"></i>
 		</div>
 		<div class="inputs">
-			{{ Form::password('password', null, ['placeholder' => 'パスワード確認用', 'name' => 'member_password', 'class' => 'input_form']) }}
+			{{ Form::password('password', ['placeholder' => 'パスワード確認用', 'name' => 'member_password', 'class' => 'input_form']) }}
 			<i class="fa fa-unlock-alt f13e" aria-hidden="true"></i>
 		</div>
 		<div class="inputs">
 			{{ Form::text('text', null, ['placeholder' => '電話番号', 'name' => 'member_phone', 'class' => 'input_form']) }}
 			<i class="fa fa-phone f095" aria-hidden="true"></i>
 		</div>
-		<div class="inputs">
+		<div class="inputs post_code">
 			{{ Form::text('text', null, ['placeholder' => '郵便番号', 'name' => 'member_postcode', 'class' => 'input_form', 'id' => 'zipcode']) }}
 			<i aria-hidden="true">〒</i>
-			<a id="zipcode_btn" style="cursor:pointer;">住所検索</a>
+			<a id="zipcode_btn" class="btn btn--orange">住所検索</a>
 		</div>
 		<div class="inputs">
 			{{ Form::text('text', null, ['placeholder' => '住所', 'name' => 'member_adress', 'class' => 'input_form', 'id' => 'address']) }}
@@ -40,5 +40,4 @@
 			{{ Form::submit('送信', ['class'=>'submit']) }}
 		</div>
 	{{ Form::close() }}
-</form>
 @endsection
